2c847df4ada6ddd61cb01f13df821169987228a9
David Buck <david@sparkytype.com>
2021-01-11 20:56:18 +1300

    Start geolocation

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,15 @@
+import { useState, useEffect } from "react";
+
 import Link from "next/link";
 
 export default function IndexPage() {
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition(function (position) {
+      console.log("Latitude is :", position.coords.latitude);
+      console.log("Longitude is :", position.coords.longitude);
+    });
+  }, []);
+
   return (
     <div>
       <Link as={`/stop/4917`} href="/stop/[sms]">
