0ccd421c9486a73467862493ec57d6a1ad55bba0
David Buck <david@sparkytype.com>
2021-01-16 23:57:11 +1300

    Fix page location

--- pages/_document.js
+++ pages/_document.js
@@ -20,7 +20,6 @@ export default class extends Document {
           />
           <meta name="apple-mobile-web-app-title" content={APP_NAME} />
           <meta name="description" content={APP_DESCRIPTION} />
-          <meta name="format-detection" content="telephone=no" />
           <meta name="mobile-web-app-capable" content="yes" />
           <meta name="theme-color" content="#FFFFFF" />
 
