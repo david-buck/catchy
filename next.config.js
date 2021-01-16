49ac520b7435ef99b3142d53bb7f107995183cea
David Buck <david@sparkytype.com>
2021-01-17 00:19:51 +1300

    Simplify PWA

--- next.config.js
+++ next.config.js
@@ -3,9 +3,6 @@ const withPWA = require("next-pwa");
 module.exports = withPWA({
   pwa: {
     disable: process.env.NODE_ENV === "development",
-    register: true,
-    scope: "/app",
-    sw: "service-worker.js",
     dest: "public",
   },
 });
