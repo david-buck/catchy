41d4b5809b8a894486ed4ea681fb69a0a4b1de96
David Buck <david@sparkytype.com>
2021-01-29 18:05:38 +1300

    Add branding, include fonts

--- pages/index.js
+++ pages/index.js
@@ -5,6 +5,8 @@ import NearbyStops from "../components/StopsNearby";
 import useSWR from "swr";
 import Head from "next/head";
 
+import Logo from "../svgs/catchy-full-logo.svg";
+
 const fetcher = (url) => fetch(url).then((res) => res.json());
 
 export default function IndexPage() {
@@ -18,7 +20,7 @@ export default function IndexPage() {
   if (!stops) return <div>Loading nearby stops...</div>;
 
   return (
-    <>
+    <div className="flex flex-col">
       <Head>
         <link
           rel="preload"
@@ -27,10 +29,13 @@ export default function IndexPage() {
           crossorigin="anonymous"
         />
       </Head>
+      <div className="mb-2">
+        <Logo width="157" height="38" title="Catchy" />
+      </div>
       <Search stops={stops} />
       <Favourites stops={stops} />
       <h2 className="text-3xl font-semibold mb-6">Stops near you</h2>
       <NearbyStops />
-    </>
+    </div>
   );
 }
