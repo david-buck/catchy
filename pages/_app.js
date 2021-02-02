50e0655c06cac53dfce16bd4ec06298335ca1a93
David Buck <david@sparkytype.com>
2021-02-03 06:46:06 +1300

    Add SW logging

--- pages/_app.js
+++ pages/_app.js
@@ -33,20 +33,20 @@ function MyApp({ Component, pageProps }) {
       const wb = window.workbox;
       // add event listeners to handle any of PWA lifecycle event
       // https://developers.google.com/web/tools/workbox/reference-docs/latest/module-workbox-window.Workbox#events
-      // wb.addEventListener("installed", (event) => {
-      //   console.log(`Event ${event.type} is triggered.`);
-      //   console.log(event);
-      // });
+      wb.addEventListener("installed", (event) => {
+        console.log(`Event ${event.type} is triggered.`);
+        console.log(event);
+      });
 
-      // wb.addEventListener("controlling", (event) => {
-      //   console.log(`Event ${event.type} is triggered.`);
-      //   console.log(event);
-      // });
+      wb.addEventListener("controlling", (event) => {
+        console.log(`Event ${event.type} is triggered.`);
+        console.log(event);
+      });
 
-      // wb.addEventListener("activated", (event) => {
-      //   console.log(`Event ${event.type} is triggered.`);
-      //   console.log(event);
-      // });
+      wb.addEventListener("activated", (event) => {
+        console.log(`Event ${event.type} is triggered.`);
+        console.log(event);
+      });
 
       // A common UX pattern for progressive web apps is to show a banner when a service worker has updated and waiting to install.
       // NOTE: MUST set skipWaiting to false in next.config.js pwa object
@@ -56,9 +56,7 @@ function MyApp({ Component, pageProps }) {
         // When `event.wasWaitingBeforeRegister` is true, a previously updated service worker is still waiting.
         // You may want to customize the UI prompt accordingly.
         if (
-          confirm(
-            "A newer version of this web app is available, reload to update?"
-          )
+          confirm("A newer version of Catchy is available, reload to update?")
         ) {
           wb.addEventListener("controlling", (event) => {
             window.location.reload();
