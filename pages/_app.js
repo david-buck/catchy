c60db0866111be7834800cfc230a5705b1acbeb3
David Buck <david@sparkytype.com>
2021-02-03 08:33:04 +1300

    Remove SW update logic until fixable

--- pages/_app.js
+++ pages/_app.js
@@ -51,28 +51,28 @@ function MyApp({ Component, pageProps }) {
       // A common UX pattern for progressive web apps is to show a banner when a service worker has updated and waiting to install.
       // NOTE: MUST set skipWaiting to false in next.config.js pwa object
       // https://developers.google.com/web/tools/workbox/guides/advanced-recipes#offer_a_page_reload_for_users
-      const promptNewVersionAvailable = (event) => {
-        // `event.wasWaitingBeforeRegister` will be false if this is the first time the updated service worker is waiting.
-        // When `event.wasWaitingBeforeRegister` is true, a previously updated service worker is still waiting.
-        // You may want to customize the UI prompt accordingly.
-        if (
-          confirm("A newer version of Catchy is available, reload to update?")
-        ) {
-          wb.addEventListener("controlling", (event) => {
-            window.location.reload();
-          });
+      // const promptNewVersionAvailable = (event) => {
+      //   // `event.wasWaitingBeforeRegister` will be false if this is the first time the updated service worker is waiting.
+      //   // When `event.wasWaitingBeforeRegister` is true, a previously updated service worker is still waiting.
+      //   // You may want to customize the UI prompt accordingly.
+      //   if (
+      //     confirm("A newer version of Catchy is available, reload to update?")
+      //   ) {
+      //     wb.addEventListener("controlling", (event) => {
+      //       window.location.reload();
+      //     });
 
-          // Send a message to the waiting service worker, instructing it to activate.
-          wb.messageSW({ type: "SKIP_WAITING" });
-        } else {
-          console.log(
-            "User rejected to reload the web app, keep using old version. New version will be automatically load when user open the app next time."
-          );
-        }
-      };
+      //     // Send a message to the waiting service worker, instructing it to activate.
+      //     wb.messageSW({ type: "SKIP_WAITING" });
+      //   } else {
+      //     console.log(
+      //       "User rejected to reload the web app, keep using old version. New version will be automatically load when user open the app next time."
+      //     );
+      //   }
+      // };
 
-      wb.addEventListener("waiting", promptNewVersionAvailable);
-      wb.addEventListener("externalwaiting", promptNewVersionAvailable);
+      // wb.addEventListener("waiting", promptNewVersionAvailable);
+      // wb.addEventListener("externalwaiting", promptNewVersionAvailable);
 
       // ISSUE - this is not working as expected, why?
       // I could only make message event listenser work when I manually add this listenser into sw.js file
