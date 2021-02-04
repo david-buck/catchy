e32c05997e3f3ce0c17babd742233ee0b2799f5f
David Buck <david@sparkytype.com>
2021-02-05 11:52:36 +1300

    New custom classes

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,14 @@ module.exports = {
     fontFamily: {
       body: ["Bus", "sans-serif"],
     },
-    extend: {},
+    extend: {
+      gridTemplateColumns: {
+        "stop-row": "2.25rem 5fr 1rem minmax(4.5rem, 1fr)",
+      },
+      maxHeight: {
+        "suggestions-container": "calc(100vh - 10rem)",
+      },
+    },
   },
   variants: {
     extend: {},
