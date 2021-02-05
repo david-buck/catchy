b476ee7e08efa909be78a9cbd2bd2a28015f871f
David Buck <david@sparkytype.com>
2021-02-05 21:25:11 +1300

    Fix "Cancelled" alignment issue

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,7 @@ module.exports = {
     },
     extend: {
       gridTemplateColumns: {
-        "stop-row": "2.25rem 5fr 1rem minmax(4.5rem, 1fr)",
+        "stop-row": "2.25rem 5fr 1rem minmax(5rem, 1fr)",
       },
       maxHeight: {
         "suggestions-container": "calc(100vh - 10rem)",
