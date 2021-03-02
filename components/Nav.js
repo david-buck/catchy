497519e4458e4a9d6967738b4d1f3acbb59fd909
David Buck <david@sparkytype.com>
2021-03-03 00:49:53 +1300

    Mobile nav

--- components/Nav.js
+++ components/Nav.js
@@ -1,24 +1,43 @@
 import Link from "next/link";
 
+import Bus from "../svgs/nav/nav-bus.svg";
+import Train from "../svgs/nav/nav-train.svg";
+import CableCar from "../svgs/nav/nav-cablecar.svg";
+import Ferry from "../svgs/nav/nav-ferry.svg";
+
 const sections = [
-  { label: "Bus", href: "/" },
-  { label: "Train", href: "/trains" },
-  { label: "Cable Car", href: "/cable-car" },
-  { label: "Ferry", href: "/ferry" },
+  { label: "Bus", href: "/", icon: Bus },
+  { label: "Train", href: "/trains", icon: Train },
+  // { label: "Cable Car", href: "/cable-car", icon: CableCar },
+  // { label: "Ferry", href: "/ferry", icon: Ferry },
 ];
+
+const SvgIcon = ({ icon }) => {
+  const Icon = icon;
+  return <Icon width={26} height={26} className="mb-1" />;
+};
 
 export default function Navigation({ current }) {
   return (
-    <nav className="flex flex-row h-20 bg-gray-400 fixed bottom-0 left-0 w-full text-sm font-semibold uppercase">
-      {sections.map((el, key) => (
-        <div className="flex-1 grid place-items-center" key={key}>
-          <Link href={el.href}>
-            <a>
-              <div>{el.label}</div>
-            </a>
-          </Link>
-        </div>
-      ))}
+    <nav className="h-20 fixed bottom-0 left-0 w-full text-sm font-semibold bg-white dark:bg-gray-800 border-gray-200 dark:border-gray-600 border-t">
+      <div className="w-full max-w-xl m-auto flex flex-row">
+        {sections.map((el, key) => (
+          <div className="flex-1 grid place-items-center h-20" key={key}>
+            <Link href={el.href}>
+              <a
+                className={`flex flex-col w-full items-center ${
+                  current === el.label.replace(" ", "").toLowerCase()
+                    ? "opacity-100"
+                    : "opacity-50"
+                }`}
+              >
+                <SvgIcon icon={el.icon} width="26" height="26" />
+                <div>{el.label}</div>
+              </a>
+            </Link>
+          </div>
+        ))}
+      </div>
     </nav>
   );
 }
